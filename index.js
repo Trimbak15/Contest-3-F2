f272386fc4050235ef00c2a1db82b51c71856443
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-29 16:40:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,118 @@
+// this function to get the menu 
+async function getMenu() {
+
+}
+
+fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
+    .then(response => response.json())
+    .then(function(data) {
+        var container = document.getElementById("container");
+        console.log(data)
+        for (let i = 0; i < data.length; i++) {
+
+
+            var divBody = `
+      <div class="card" >
+          <img src="${data[i].imgSrc}" class="card-img" alt="" >
+          <div class="content">
+              <h2>${data[i].id}</h2>
+              <h3>${data[i].name}</h3>
+              <h3> Price: ${data[i].price}</h3>
+              
+               <a href="#" onclick="takeOrder(${data[i].id},'${data[i].imgSrc}','${data[i].name}',${data[i].price})"> Order now</a>
+          </div>
+      </div>`;
+            container.innerHTML += divBody;
+        }
+
+
+    })
+    .catch(error => console.error(error));
+
+
+
+
+
+
+// Function to take the order
+function takeOrder() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            // console.log(orderid,imglink,proName,proPrice);
+            // console.log(orderid.children[1].children[2].innerText);
+            const burgers = ["Burger 1", "Burger 2", "Burger 3"];
+            //  const order=burgers;
+            const order = {
+                burger1: burgers[Math.floor(Math.random() * burgers.length)],
+                burger2: burgers[Math.floor(Math.random() * burgers.length)],
+                burger3: burgers[Math.floor(Math.random() * burgers.length)]
+            };
+
+            resolve(order);
+        }, 2500);
+    });
+}
+
+// order preparation
+function orderPrep() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const orderStatus = true;
+            const paid = false;
+            resolve({ orderStatus, paid });
+        }, 1500);
+    });
+}
+
+// pay the order
+function payOrder() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const orderStatus = true;
+            const paid = true;
+            resolve({ orderStatus, paid });
+        }, 1000);
+    });
+}
+
+//thank alert
+function thankyouFnc() {
+    alert('Thank you for eating with us today!');
+}
+
+// see the Promise chaining
+getMenu()
+    .then(() => takeOrder())
+    .then(order => {
+        console.log(order);
+
+        return orderPrep();
+    })
+    .then(status => {
+        console.log(status);
+        return payOrder();
+    })
+    .then(status => {
+        console.log(status);
+        thankyouFnc();
+    })
+    .catch(error => console.error(error));
+
+//   this Async/await
+async function runOrder() {
+    try {
+        await getMenu();
+        const order = await takeOrder();
+        console.log(order);
+        const status = await orderPrep();
+        console.log(status);
+        const payment = await payOrder();
+        console.log(payment);
+        thankyouFnc();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+runOrder();
